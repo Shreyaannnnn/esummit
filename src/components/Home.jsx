c669186c51fa08fc7ffaec670e33b25ac5d7317e
Aakash Rajbhar <akash.kumarajbhar@gmail.com>
2024-03-05 21:58:10 +0530

    made navbar responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import video from '../assets/video3.mov'
+import './Home.css'
 
 
 const Home = () => {
   return (
     <div  className="container">
-        <video src={video}  autoPlay loop muted/>
+       <video autoPlay loop muted>
+            <source src={video} type="video/mp4"/>
+            Your browser does not support the video tag.
+        </video>
 
+        <div className="home_content">
+          <h2>Discover Unforgettable Events Near You!</h2>
+          <button><a href="/">Register Now</a></button>
+        </div>
 
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,51 @@
-import React from "react";
-import './Navbar.css'
+import React, { useState } from "react";
+import "./Navbar.css";
+import hamburger from "../assets/hamburger.svg";
+import cross from "../assets/cross.svg";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+    console.log(toggle);
+    console.log("clicked");
+  };
+
   return (
     <nav>
       <div className="nav_logo">
         <h2>Logo</h2>
       </div>
       <div className="nav_links_div">
-        <ul className="nav_links_ul">
-            <li><a href="#home" className="nav_link">Home</a></li>
-            <li><a href="#about" className="nav_link">About</a></li>
-            <li><a href="#events" className="nav_link">Events</a></li>
-            <li><a href="#contact" className="nav_link">Contact</a></li>
+        <ul className={`nav_links_ul ${toggle ? "show" : ""}`}>
+          <div className="cross">
+            <img src={cross} alt="close" onClick={handleToggle} />
+          </div>
+          <li>
+            <a href="#home" className="nav_link">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="nav_link">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#events" className="nav_link">
+              Events
+            </a>
+          </li>
+          <li>
+            <a href="#contact" className="nav_link">
+              Contact
+            </a>
+          </li>
         </ul>
+        <div className="nav_menu" onClick={handleToggle}>
+          <img src={hamburger} alt="nav menu" />
+        </div>
       </div>
     </nav>
   );
