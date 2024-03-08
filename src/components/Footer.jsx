34a69fc00fa6dd4ec79f7e2876e79ebd7f907fa6
Aakash Rajbhar <akash.kumarajbhar@gmail.com>
2024-03-08 14:04:16 +0530

    modified events section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,18 +5,18 @@ import ecell_logo from '../assets/ecell_logo.png'
 
 const Footer = () => {
   return (
-    <footer className="footer-container">
+    <footer className="footer-container"  id="contact">
       <div className="footer-content">
         <div className="footer-section logo-section">
           <img src= {ecell_logo} alt="Logo" className="logo" />
         </div>
         <div className="footer-section quick-links-section">
-          <h4>Quick Links</h4>
+          <h3>Quick Links</h3>
           <ul>
             <li><a href="#home">Home</a></li>
             <li><a href="#about">About</a></li>
-            <li><a href="#services">Events</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li><a href="#events">Events</a></li>
+            {/* <li><a href="#contact">Contact</a></li> */}
           </ul>
         </div>
         <div className="footer-section contact-section">
