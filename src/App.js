e90db9f3d7837db2a76d12f8cc18fda6e2e5654e
Aakash Rajbhar <akash.kumarajbhar@gmail.com>
2024-03-09 15:11:21 +0530

    added traders tussle page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import Navbar from '../src/components/Navbar';
-import Home from '../src/components/Home';
-import About from './components/About';
-import Events from './components/Events';
-import Socials from  './components/Socials';
-import Footer from './components/Footer';
+// import Navbar from '../src/components/Navbar';
+// import Home from '../src/components/Home';
+// import About from './components/About';
+// import Events from './components/Events';
+// import Socials from  './components/Socials';
+// import Footer from './components/Footer';
 import Ideathon from './components/Ideathon/Main'
 import BattleOfBrands from './components/BattleOfBrands/BattleOfBrands';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from './components/Homepage/Homepage';
 import NFTRush from './components/NFTRush/NFTRush';
+import TradersTussle from './components/Traders-Tussle/TradersTussle';
 
 const App = () => {
   return (
@@ -23,6 +24,8 @@ const App = () => {
           <Route path="/battleofbrands" element={<BattleOfBrands />} />
           {/* <Route path="/nftrush" component={NFTRush} /> */}
           <Route path='/nftrush' element={<NFTRush/>} />
+          {/* <Route path="/nftrush" component={NFTRush} /> */}
+          <Route path='/traderstussle' element={<TradersTussle/>} />
         </Routes>
         {/* Components outside the Switch will be rendered on all pages */}
 
